4a48cbddb362bfb3f86bf7d0d00831373cf93831
Alan Ng <alanngyf@gmail.com>
2015-06-11 20:50:32 -0700

    find an address under batteries & lamp section

--- battery_and_lamp_disposal_sites_in_mountain_view/extract_address.py
+++ battery_and_lamp_disposal_sites_in_mountain_view/extract_address.py
@@ -1,6 +1,9 @@
 import urllib2
 import sys
+import re
 data = urllib2.urlopen("http://www.mountainview.gov/depts/pw/recycling/hazard/default.asp")
-
-for lines in data.readlines():
-    sys.stdout.write(lines)
+page = data.read()
+page = page[page.find("Batteries &amp; Lamps"):page.find("BATTERIES ONLY")]
+address = re.findall("</strong>[^<]+", page)
+for element in address:
+	print re.sub("</strong>", "", element)
